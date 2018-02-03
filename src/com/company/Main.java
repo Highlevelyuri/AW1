8e2e61038878e1257639a4bd7654d44a114716b8
Highlevelyuri <kriucov61944zce3.com@gmail.com>
2018-02-03 11:42:45 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        for (double x =1.0 ; x<=5; x+=0.5  ){
+            System.out.println(x + "\t" +( 3*(Math.pow(x,2))-5));
+
+        }
+    }
+
+}
+
+
+
+
+
+
+
